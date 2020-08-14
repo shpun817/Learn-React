a3b670a942387ad110bc36e9d8e9f643f79bc8a4
Sanford Pun <punsanford@gmail.com>
2020-08-14 20:19:43 +0800

    Assignment 1

---
+++ confusion/src/components/DishdetailComponent.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import { Card, CardImg, CardBody, CardTitle, CardText } from 'reactstrap';
+
+class DishDetail extends Component {
+
+	constructor(props) {
+		super(props);
+	}
+
+	renderDish(dish) {
+		return (
+			<Card>
+				<CardImg width="100%" src={dish.image} alt={dish.name} />
+				<CardBody>
+					<CardTitle>{dish.name}</CardTitle>
+					<CardText>{dish.description}</CardText>
+				</CardBody>
+			</Card>
+		);
+	}
+
+	renderComments(dish) {
+		const comments = dish.comments.map((comment) => {
+			return (
+				<div key={comment.id}>
+					<p>{comment.comment}</p>
+					<p>-- {comment.author}, {comment.date}</p>
+				</div>
+			);
+		});
+		if (comments != null) {
+			return (
+				<div>
+					<h4>Comments</h4><br />
+					{ comments }
+				</div>
+			);
+		} else {
+			return (<div>)</div>);
+		}
+
+	}
+
+	render() {
+		const dish = this.props.dish;
+		if (dish != null) {
+			return (
+				<>
+					<div className="col-12 col-md-5 m-1">
+						{ this.renderDish(dish) }
+					</div>
+					<div className="col-12 col-md-5 m-1">
+						{ this.renderComments(dish) }
+					</div>
+				</>
+			);
+		} else {
+			return (<div></div>);
+		}
+	}
+}
+
+export default DishDetail;
--- confusion/src/components/MenuComponent.js
+++ confusion/src/components/MenuComponent.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import DishDetail from './DishdetailComponent';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
 
 class Menu extends Component {
@@ -20,13 +21,7 @@ class Menu extends Component {
 	renderDish(dish) {
 		if (dish != null) {
 			return(
-				<Card>
-					<CardImg width="100%" src={dish.image} alt={dish.name} />
-					<CardBody>
-						<CardTitle>{dish.name}</CardTitle>
-						<CardText>{dish.description}</CardText>
-					</CardBody>
-				</Card>
+				<DishDetail dish={dish} />
 			);
 		} else {
 			return(<div></div>);
