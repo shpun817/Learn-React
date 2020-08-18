7b8607a2ff6ab9ae62bab5837cce6b0bd206833c
Sanford Pun <punsanford@gmail.com>
2020-08-18 21:53:41 +0800

    Assignment 2

--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -5,6 +5,7 @@ import Contact from './ContactComponent';
 import DishDetail from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import About from './AboutComponent';
 import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
 import { LEADERS } from '../shared/leaders';
@@ -34,6 +35,12 @@ class Main extends Component {
 			);
 		};
 
+		const AboutPage = () => {
+			return(
+				<About leaders={this.state.leaders} />
+			);
+		}
+
 		const DishWithId = ({match}) => {
 			return(
 				<DishDetail dish={this.state.dishes.filter( (dish) => dish.id == match.params.dishId)[0]}
@@ -49,6 +56,7 @@ class Main extends Component {
 					<Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
 					<Route path="/menu/:dishId" component={DishWithId} />
 					<Route exact path="/contactus" component={Contact} />
+					<Route exact path="/aboutus" component={AboutPage} />
 					<Redirect to="/home" />
 				</Switch>
 				<Footer />
