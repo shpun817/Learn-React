2197a1e1e209e49292b6665ecd3cc7f6751be4f7
Sanford Pun <punsanford@gmail.com>
2020-08-20 18:33:04 +0800

    Redux Thunk

--- confusion/src/components/DishdetailComponent.js
+++ confusion/src/components/DishdetailComponent.js
@@ -3,6 +3,7 @@ import { Card, CardImg, CardBody, CardTitle, CardText, Breadcrumb, BreadcrumbIte
 	Modal, ModalHeader, ModalBody, Button, Row, Col, Label } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Loading } from './LoadingComponent';
 
 const minLength = (len) => (val) => (val) && (val.length >= len);
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -134,6 +135,24 @@ function RenderComments({dishComments, addComment, dishId}) {
 }
 
 const DishDetail = (props) => {
+	if (props.isLoading) {
+		return(
+			<div className="container">
+				<div className="row">
+					<Loading />
+				</div>
+			</div>
+		);
+	} else if (props.errMess) {
+		return(
+			<div className="container">
+				<div className="row">
+					<h4>{props.errMess}</h4>
+				</div>
+			</div>
+		);
+	}
+
 	const dish = props.dish;
 	const comments = props.comments;
 	if (dish != null) {
